7a7c2869738ee8a900cd1ef009435798e93f9c25
pgxtips <brandongill123@gmail.com>
2022-03-29 07:34:56 +0100

    Problem 1 Solved

--- ProjectEuler/ProjectEuler.cpp
+++ ProjectEuler/ProjectEuler.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+// Problem 1 - Multiples of 3 or 5
+
+
+int problem1 () {
+    int sum = 0;
+    for (int i = 0; i < 1000; i++) {
+        if ((i % 3 == 0) || (i % 5 == 0)) {
+            sum+=i;
+        }
+    }
+    return sum;
+}
+
 int main()
 {
-    std::cout << "" << std::endl;
+    std::cout << "Problem 1 answer: "<< problem1() << std::endl;
 }
