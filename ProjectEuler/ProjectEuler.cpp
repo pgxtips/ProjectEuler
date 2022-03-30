6d42abf8ad44d776bf71f40a2ae5f8a865e960e0
pgxtips <brandongill123@gmail.com>
2022-03-30 02:58:41 +0100

    Problem 4 Solved

--- ProjectEuler/ProjectEuler.cpp
+++ ProjectEuler/ProjectEuler.cpp
@@ -35,7 +35,7 @@ void problem2() {
 // Problem 3 - Largest Prime Factor
 void problem3() {
 
-    std::vector<int> vect;
+    std::vector<long long int> vect;
     long long int n = 600851475143;
 
     // loop until n is not divisible by 2
@@ -59,13 +59,52 @@ void problem3() {
     if (n > 2)
         vect.push_back(n);
 
-    std::cout << "Problem 2 answer: " << vect.back() << std::endl;
+    std::cout << "Problem 3 answer: " << vect.back() << std::endl;
 }
 
+// Problem 4 - Largest palindrome made from the product of 2, 3-digit numbers
+bool isPalindrome(int n) {
+    // to reverse number
+    int nRev = 0;
+    int remainder = 0;
+    int temp = n;
+
+    while (temp != 0) {
+        remainder = temp % 10;
+        nRev = nRev * 10 + remainder;
+        temp = temp / 10;
+    }
+
+    // check if the reverse is equal to number
+    if (n == nRev) {return true;}
+    return false;
+}
+
+void problem4() {
+    int largest = 0;
+    int pair[2] = { 0,0 };
+    int product = 0;
+    // Create 2 loop that works from the bottom of the 3 range range
+    // and one that works from the top range down
+    for (int i = 0; i < 999; i++) {
+        for (int j = 999; j > 0; j--) {
+            product = i * j;
+            if (isPalindrome(product) && product > largest) {
+                largest = product;
+                pair[0] = i;
+                pair[1] = j;
+            }
+        }
+    }
+    std::cout << "Problem 4 answer: ";
+    std::cout << pair[0] << " x " << pair[1];
+    std::cout << " = " << largest << std::endl;
+}
 
 int main()
 {
     problem1();
     problem2();
     problem3();
+    problem4();
 }
